986ba8e99d82e7d733b4fd391bc21d2b8b7b5a58
plusmancn <plusmancn@gmail.com>
2017-02-25 23:09:08 +0800

    feat:  添加组件测试列表

--- UiLibrary/Demo.js
+++ UiLibrary/Demo.js
@@ -8,4 +8,72 @@
  *
  * Demo 页面入口文件
  */
-export { default } from './TabBar/test/index.js';
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    ListView,
+    TouchableHighlight,
+    View,
+    Text
+} from 'react-native';
+
+// 加载组件
+import TabBar from './TabBar/test/index.js'
+
+class Demo extends Component {
+    constructor() {
+        super();
+
+        this.ds = new ListView.DataSource({
+            rowHasChanged: function(r1, r2) {
+                return r1.name !== r2.name;
+            }
+        });
+
+        this.components = [
+            {
+                name: 'TabBar',
+                compoent: TabBar
+            }
+        ];
+    }
+
+    _renderRow(row) {
+        return (
+            <TouchableHighlight
+                onPress={() => {
+                }}
+            >
+                <View
+                    style={styles.listItem}
+                >
+                    <Text
+
+                    >{row.name}</Text>
+                </View>
+            </TouchableHighlight>
+        );
+    }
+
+    render() {
+        return (
+            <ListView
+                style={styles.container}
+                dataSource={this.ds.cloneWithRows(this.components)}
+                renderRow={this._renderRow}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 22,
+        flex: 1
+    },
+    listItem: {
+        backgroundColor: '#fff'
+    }
+});
+
+export default Demo;
